6c2e870d2824e50a522acf3a01c7489eabdf5b15
ikotun-dev <danlogan2003@gmail.com>
2024-05-24 11:29:08 +0100

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
